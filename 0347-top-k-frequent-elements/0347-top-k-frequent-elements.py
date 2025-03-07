aff4d23f0d0f2b789b24b50347caa857319749d2
Jenny <77416265+yjennykim@users.noreply.github.com>
2025-03-07 01:23:38 -0600

    Time: 3 ms (91.14%), Space: 21.3 MB (62.28%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,8 +3,5 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Count the frequency of each element in nums
         count = Counter(nums)
-        
-        # Get the k most common elements (returns a list of tuples: (element, frequency))
         return [item[0] for item in count.most_common(k)]
